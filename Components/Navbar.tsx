6481a1da7b5f49f009bbc744b805c173f08b2aa2
sand-storm-17 <sandeeepkumaar.0034@gmail.com>
2024-10-02 15:30:07 +0530

    folder name changed

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -1,6 +1,21 @@
+import Link from "next/link";
+
 const Navbar = () => {
     return <div>
-        Navbar
+        <div>
+            <div>
+                <h1>Sandeep</h1>
+            </div>
+            <div>
+                <Link href="">Services</Link>
+                <Link href="">Experience</Link>
+                <Link href="">About Me</Link>
+                <Link href="">Social Media Links</Link>
+            </div>
+        </div>
+        <div>
+            <button>See my work</button>
+        </div>
     </div>
 }
 
